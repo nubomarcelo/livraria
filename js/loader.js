9afd9d676fadaed69828128c3aad26c7d09e582d
Marcelo Ramos <marcelo@nuboecnologia.com.br>
2017-10-23 13:09:59 -0400

    Finalização do load content e adição das páginas do load

--- js/loader.js
+++ js/loader.js
@@ -1,15 +1,23 @@
-//loader do conteúdo principal da página
-$(document).ready(function(){
-    $("#content").load("conteudos/corpo-home.html");
-});
-//loader do topo e rodapé da página
+
 $(document).ready(function(){
     $("#topo").load("conteudos/_header.html");
     $("#rodape").load("conteudos/_footer.html");
 });
 
-//loader para o filtro de conteúdo dos itens
-$("ul#na li a").click(function(){
-    var page = $(this).attr("href");
-    $("#content").load(page+".html");
+$(document).ready(function(){
+    $("#content").load("conteudos/corpo-home.html");
+    $("ul#menu li a").click(function(){
+        var pagina = $(this).attr('href');
+        $('#content').load('conteudos/'+pagina+'.html');
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("#content").load("conteudos/anjosdemons.html");
+    $("ul#menu li a").click(function(){
+        var pagina = $(this).attr('href');
+        $('#content').load('conteudos/'+pagina+'.html');
+        return false;
+    });
 });
